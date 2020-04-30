111d211dfc3e3414612ccbd7e2f18546198b3a1f
xhliu <xhliu@dfoucspace.com>
2020-04-30 14:29:27 +0800

    add k8s

---
+++ k8s/0-install-k8s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 在 master 节点和 worker 节点都要执行
+# 最后一个参数 1.18.2 用于指定 kubenetes 版本，支持所有 1.18.x 版本的安装
+# 腾讯云 docker hub 镜像
+# export REGISTRY_MIRROR="https://mirror.ccs.tencentyun.com"
+# DaoCloud 镜像
+# export REGISTRY_MIRROR="http://f1361db2.m.daocloud.io"
+# 华为云镜像
+# export REGISTRY_MIRROR="https://05f073ad3c0010ea0f4bc00b7105ec20.mirror.swr.myhuaweicloud.com"
+# 阿里云 docker hub 镜像
+export REGISTRY_MIRROR=https://registry.cn-hangzhou.aliyuncs.com
+curl -sSL https://kuboard.cn/install-script/v1.18.x/install_kubelet.sh | sh -s 1.18.2
