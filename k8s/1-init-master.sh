5aa41f0baa07e1f4b5739a65337a8a1943c66341
xhliu <xhliu@dfoucspace.com>
2020-04-30 14:37:35 +0800

    feat: add init master

--- k8s/1-init-master.sh
+++ k8s/1-init-master.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # 只在 master 节点执行
+# 替换 x.x.x.x 为 master 节点实际 IP（请使用内网 IP）
+# export 命令只在当前 shell 会话中有效，开启新的 shell 窗口后，如果要继续安装过程，请重新执行此处的 export 命令
+export MASTER_IP=192.168.10.165
+# 替换 apiserver.demo 为 您想要的 dnsName
+export APISERVER_NAME=apiserver.admin.df
+# Kubernetes 容器组所在的网段，该网段安装完成后，由 kubernetes 创建，事先并不存在于您的物理网络中
+export POD_SUBNET=10.100.0.1/16
+echo "${MASTER_IP}    ${APISERVER_NAME}" >> /etc/hosts
+
 
 # 脚本出错时终止执行
 set -e
